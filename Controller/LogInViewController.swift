0b711f4554967b9d087a0ca44ab1df6ad0268c66
원종필 <daori@yahoo.com>
2017-12-31 17:41:33 +0900

    Setup LogIn Screen UI

---
+++ Controller/LogInViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LogInViewController.swift
+//  Walkie
+//
+//  Created by wonjongpill on 2017. 12. 31..
+//  Copyright © 2017년 Jayron Cena. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logIn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func signUp(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
